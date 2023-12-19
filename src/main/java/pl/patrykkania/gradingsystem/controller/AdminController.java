25658acb8f9d3e6a06baf0e85ddf19874593b7c9
JaPatryk <s094236@student.tu.kielce.pl>
2023-12-19 23:27:43 +0100

    -Student add by admin, -validate student pesel -the student model requires a relationship with the class table (non-competitive)

--- src/main/java/pl/patrykkania/gradingsystem/controller/AdminController.java
+++ src/main/java/pl/patrykkania/gradingsystem/controller/AdminController.java
@@ -1,13 +1,46 @@
 package pl.patrykkania.gradingsystem.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.patrykkania.gradingsystem.model.Student;
+import pl.patrykkania.gradingsystem.service.StudentService;
+
 
 
 @Controller
 public class AdminController {
+    @Autowired
+    private StudentService studentService;
+
     @GetMapping("/admin")
     String Home() {
         return "home";
     }
+
+
+    @GetMapping("/admin/add-student")
+    String AddStudent() {
+        return "admin/add-student";
+    }
+
+    @PostMapping("admin/add-student")
+    public void addStudent(@ModelAttribute Student student, ModelMap model) {
+        try {
+            studentService.save(student);
+            model.addAttribute("message", "Student został pomyślnie dodany");
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("error", e.getMessage());
+        } catch (Exception e) {
+            model.addAttribute("error", "Wystąpił nieoczekiwany błąd.");
+        }
+
+//        return "redirect:/admin/add-student";
+
+    }
+
+
 }
